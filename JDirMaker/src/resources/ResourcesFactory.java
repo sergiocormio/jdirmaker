a72cef50be8a870a237c7d7c2467d3fb4552940d
sergiocormio <sergiocormio@5e1acb8d-b6c6-bdcd-9657-483d3a5f7c47>
2014-04-01 04:20:24 +0000

    Issue 1: Destination Folder panel, first implementation complete.

--- JDirMaker/src/resources/ResourcesFactory.java
+++ JDirMaker/src/resources/ResourcesFactory.java
@@ -19,4 +19,15 @@ public class ResourcesFactory {
 			return new ImageIcon(ResourcesFactory.class.getResource("tick.png"));
 		}
 		
+		//Open Folder Icon
+		public static ImageIcon getOpenFolderIcon(){
+			return new ImageIcon(ResourcesFactory.class.getResource("folder-horizontal-open.png"));
+		}
+		
+		//Choose Folder Icon
+		public static ImageIcon getChooseFolderIcon(){
+			return new ImageIcon(ResourcesFactory.class.getResource("folder-search-result.png"));
+		}
+				
+		
 }
